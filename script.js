8d2be7c6b84bc85e656cdc6cdc951c846f3da57d
arnav2000agr <92395924+arnav2000agr@users.noreply.github.com>
2021-10-27 18:34:51 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,64 @@
+const button = document.querySelector('input[type="button"]');
+const list = document.querySelector("ul");
+const input = document.querySelector("input[type='text']");
+var today = new Date();
+var date = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
+document.getElementById("date").innerHTML=date;
+const newTodos = document.querySelectorAll('li');
+const form = document.querySelector('form');
+let newTodo;
+let listItemText;
+let List = [];
+function add() {
+    listItemText = document.createElement("span");
+    listItemText.innerText = input.value;
+   const checkBox = document.createElement('input');
+   checkBox.type = "checkbox";
+   checkBox.addEventListener('click', function(){
+   if(this.checked ){ 
+       listItemText.style.textDecoration = "none";
+   }           
+   })
+   const deleteButton = document.createElement('button');
+   deleteButton.innerText = "Delete";
+   deleteButton.classList.add("delete");
+   deleteButton.addEventListener('click', function(e){
+       let li = this.parentElement;
+       li.remove()
+   })
+   newTodo = document.createElement("li");
+   newTodo.append(listItemText, checkBox, deleteButton);
+   newTodo.setAttribute('data-id', "text");
+   list.appendChild(newTodo);
+   input.value = "";
+   List.push({
+    task: listItemText.innerText,
+    done: false
+  })
+  localStorage.setItem('todos', JSON.stringify(List));
+  JSON.parse(localStorage.getItem("todos"));
+}
+if(JSON.parse(localStorage.getItem("todos")) == []) {
+  console.log('fff')
+}
+button.addEventListener('click', function(event){
+  if(input.value==""){
+  alert("Give some task name!!");
+  }
+  else
+  {
+    add();
+  }
+});
+input.addEventListener("keydown", function(event) {  
+    if (event.keyCode === 13  ) {
+      if(input.value!=""){
+      event.preventDefault();
+      add();
+    }
+    else{
+      event.preventDefault();
+      alert("Give some task name!!");
+    }
+  }
+  });
